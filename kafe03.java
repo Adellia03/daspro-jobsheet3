e8a18de9c666a3e48cc7f3db453bca4d2d2fc190
Adellia03 <salsadellia736@gmail.com>
2024-09-11 09:26:35 +0700

    Adellia Salsa Al Barra

--- kafe03.java
+++ kafe03.java
@@ -1,13 +1,15 @@
-import.java.util.Scanner;
+import java.util.Scanner;
 
 public class kafe03{
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         boolean keanggotaan;
         int jmlKopi, jmlTeh, jmlRoti;
         double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0;
         float diskon = 10 / 100f;
+        double totalHarga;
+        double nominalBayar;
 
         System.out.print("Masukkan keanggotaan (true/false): ");
         keanggotaan = input.nextBoolean();
